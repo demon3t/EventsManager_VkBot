369d233e60f5d6c3ad103a2dbfc114088083af5f
sadyrin.inc@outlook.com <sadyrin.inc@outlook.com>
2022-10-15 17:57:12 +0300

    add table and procedures

---
+++ Database/Events.sql
@@ -0,0 +1,22 @@
+﻿/*
+В базе данных должна быть предусмотрена файловая группа MEMORY_OPTIMIZED_DATA,
+чтобы можно было создать оптимизированный для памяти объект.
+
+Число сегментов должно быть задано как примерно в два раза 
+превышающее максимально ожидаемое количество уникальных значений в 
+ключе индекса с округлением до ближайшего четного числа.
+*/
+
+CREATE TABLE [dbo].[Events]
+(
+    [Id]         INT            IDENTITY (1, 1) NOT NULL PRIMARY KEY NONCLUSTERED HASH WITH (BUCKET_COUNT = 131072),
+    [Author]     NVARCHAR (20)  NOT NULL,
+    [CreateTime] DATETIME       NOT NULL,
+    [Actual]     BIT            NOT NULL,
+    [Name]       NVARCHAR (100) NOT NULL,
+    [Place]      NVARCHAR (50)  NOT NULL,
+    [Seats]      INT            NOT NULL,
+    [Describe]   NVARCHAR (MAX) NOT NULL,
+    [StartTime]  DATETIME       NOT NULL,
+    [EndTime]    DATETIME       NOT NULL,
+) WITH (MEMORY_OPTIMIZED = ON)
---
+++ Database/EventsProcedures/EventSetParams.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE [dbo].[EventSetParams]
+    @Id INT,
+	@Actual BIT = NULL,
+ 	@Name NVARCHAR(20) = NULL,
+	@Place NVARCHAR(50) = NULL,
+	@Seats INT = NULL,
+	@Describe NVARCHAR(MAX) = NULL,
+	@StartTime DATETIME = NULL,
+	@EndTime DATETIME = NULL
+AS
+BEGIN
+
+	IF (@Actual IS NOT NULL) BEGIN
+	UPDATE Events SET Actual = @Actual
+	WHERE Id = @Id END
+
+	IF (@Name IS NOT NULL) BEGIN
+	UPDATE Events SET Name = @Name
+	WHERE Id = @Id END
+
+	IF (@Place IS NOT NULL) BEGIN
+	UPDATE Events SET Place = @Place
+	WHERE Id = @Id END
+
+	IF (@Seats IS NOT NULL) BEGIN
+	UPDATE Events SET Seats = @Seats
+	WHERE Id = @Id END
+
+	IF (@Describe IS NOT NULL) BEGIN
+	UPDATE Events SET Describe = @Describe
+	WHERE Id = @Id END
+
+	IF (@StartTime IS NOT NULL) BEGIN
+	UPDATE Events SET StartTime = @StartTime
+	WHERE Id = @Id END
+
+	IF (@EndTime IS NOT NULL) BEGIN
+	UPDATE Events SET EndTime = @EndTime
+	WHERE Id = @Id END
+	
+END
